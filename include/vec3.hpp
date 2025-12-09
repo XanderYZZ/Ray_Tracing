78da43d3ab45cd679eeb6eee2740c0df752c2b1c
Xander <87785817+XanderYZZ@users.noreply.github.com>
2025-12-08 21:04:27 -0700

    Update

---
+++ include/vec3.hpp
@@ -0,0 +1,70 @@
+#ifndef VEC3_HPP
+#define VEC3_HPP
+
+#include <cmath>
+#include <iostream> 
+#include <array>
+
+class vec3 {
+public:
+    vec3() : xyz{0, 0, 0} {};
+    vec3(double x, double y, double z) : xyz{x, y, z} {};
+    
+    std::array<double, 3> GetXYZ() const { 
+        return xyz; 
+    }
+
+    double x() const { return xyz[0]; }
+    double y() const { return xyz[1]; }
+    double z() const { return xyz[2]; }
+
+    vec3 operator-() const { return vec3(-xyz[0], -xyz[1], -xyz[2]); }
+    double operator[](int i) const { return xyz[i]; }
+    double &operator[](int i) { return xyz[i]; } // To allow write operations by reference. 
+
+    vec3 &operator+=(const vec3 &v) {
+        xyz[0] += v.x();
+        xyz[1] += v.y();
+        xyz[2] += v.z();
+
+        return *this;
+    }
+    
+    vec3 &operator*=(double t) {
+        xyz[0] *= t;
+        xyz[1] *= t;
+        xyz[2] *= t;
+
+        return *this;
+    }
+
+    vec3 &operator/=(double t) {
+        return *this *= 1/t;
+    }
+
+    double length() const {
+        return std::sqrt(length_squared());
+    }
+
+    double length_squared() const {
+        return std::pow(xyz[0], 2) + std::pow(xyz[1], 2) + std::pow(xyz[2], 2);
+    }
+
+private:
+    std::array<double, 3> xyz;
+};
+
+// Operator declarations outside of the class definition are below.
+std::ostream& operator<<(std::ostream&, const vec3&);
+vec3 operator+(const vec3&, const vec3&);
+vec3 operator-(const vec3&, const vec3&);
+vec3 operator*(const vec3&, const vec3&);
+vec3 operator*(double, const vec3&);
+vec3 operator*(const vec3&, double);
+vec3 operator/(const vec3&, double);
+
+double dot(const vec3&, const vec3&);
+vec3 cross(const vec3&, const vec3&);
+vec3 unit_vector(const vec3&);
+
+#endif 
---
+++ src/vec3.cpp
@@ -0,0 +1,44 @@
+#include "vec3.hpp"
+#include <ostream>
+
+std::ostream &operator<<(std::ostream &out, const vec3 &v) {
+    return out << "(" << v.x() << ", " << v.y() << ", " << v.z() << ")";
+} 
+
+vec3 operator+(const vec3 &u, const vec3 &v) {
+    return vec3(u.x() + v.x(), u.y() + v.y(), u.z() + v.z());
+}
+
+vec3 operator-(const vec3 &u, const vec3 &v) {
+    return vec3(u.x() - v.x(), u.y() - v.y(), u.z() - v.z());
+}
+
+vec3 operator*(const vec3 &u, const vec3 &v) {
+    return vec3(u.x() * v.x(), u.y() * v.y(), u.z() * v.z());
+}
+
+vec3 operator*(double t, const vec3 &v) {
+    return vec3(t * v.x(), t * v.y(), t * v.z());
+}
+
+vec3 operator*(const vec3 &v, double t) {
+    return t * v;
+}
+
+vec3 operator/(const vec3 &v, double t) {
+    return (1/t) * v;
+}
+
+double dot(const vec3 &u, const vec3 &v) {
+    return u.x() * v.x() + u.y() * v.y() + u.z() * v.z();
+}
+
+vec3 cross(const vec3 &u, const vec3 &v) {
+    return vec3(u.y() * v.z() - u.z() * v.y(), 
+                u.z() * v.x() - u.x() * v.z(),
+                u.x() * v.y() - u.y() * v.x());
+}
+
+vec3 unit_vector(const vec3 &v) {
+    return v / v.length();
+}
